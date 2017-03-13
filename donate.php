058f42d4130903681fc7d24a2ed126f3fca30764
Andrew Grant <inarus13@gmail.com>
2017-03-13 14:39:53 +0000

    Uploaded main donate files
    SVG icons; master CSS file used for main design and testing. index.php contains imitation of freeUKgenealogy website and the donate button html code. donate.php contains only the donate button html code.

---
+++ donate.php
@@ -0,0 +1,8 @@
+<div id="center">
+  <a href="/test/freeGen/bmd" class="freeGen" id="donate">
+    <span class="btn-donate" id="donateOverlay"><?php include($_SERVER['DOCUMENT_ROOT'] . '/test/FreeGen/donateOverlay.svg');?></span>
+    <span class="btn-donate" id="donateCoin"><?php include($_SERVER['DOCUMENT_ROOT'] . '/test/FreeGen/donateCoin.svg');?></span>
+    <span class="btn-donate" id="donateBox"><?php include($_SERVER['DOCUMENT_ROOT'] . '/test/FreeGen/donateBox.svg');?></span>
+    <span class="btn-donate" id="donateBtn"><?php include($_SERVER['DOCUMENT_ROOT'] . '/test/FreeGen/donateBtn.svg');?>Donate</span>
+  </a>
+</div>
